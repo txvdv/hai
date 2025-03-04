86e45b5d8a4eaef4fe090e5564a2e284f441a937
txvdv <txvdv@protonmail.com>
2025-03-04 09:09:47 +0100

    Initial implementation of Document Service

--- packages/document-service/src/lib/document-service.ts
+++ packages/document-service/src/lib/document-service.ts
@@ -1,3 +1,44 @@
 export function documentService(): string {
   return 'document-service';
 }
+
+export type Document = {
+  id: string
+  content: string
+};
+
+export class DocumentService {
+  documents: Document[] = [];
+
+  async getDocument(id: string) {
+    return this.documents.find(doc => doc.id === id);
+  }
+
+  async getDocuments() {
+    return this.documents;
+  }
+
+  async createDocument(content: string) {
+    const document = {
+      id: generateShortUniqueId(),
+      content
+    };
+    this.documents.push(document);
+    return document;
+  }
+
+  async updateDocument(id: string, content: string) {
+    const document = this.documents.find(doc => doc.id === id);
+    if (document) {
+      document.content = content;
+    }
+  }
+
+  async deleteDocument(id: string) {
+    this.documents = this.documents.filter(doc => doc.id !== id);
+  }
+}
+
+function generateShortUniqueId(): string {
+  return [...Array(9)].map(() => Math.random().toString(36)[2]).join('');
+}
