0dcff87fe67a692642e7a8d6b2baf7234694494c
txvdv <txvdv@protonmail.com>
2025-03-04 08:52:20 +0100

    Add document service package

---
+++ packages/document-service/src/lib/document-service.spec.ts
@@ -0,0 +1,7 @@
+import { documentService } from './document-service.js';
+
+describe('documentService', () => {
+  it('should work', () => {
+    expect(documentService()).toEqual('document-service');
+  });
+});
---
+++ packages/document-service/src/lib/document-service.ts
@@ -0,0 +1,3 @@
+export function documentService(): string {
+  return 'document-service';
+}
