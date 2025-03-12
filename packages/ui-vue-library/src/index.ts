257faa4a3130e4f8f0f7001bf85c3a8f51395a89
txvdv <txvdv@protonmail.com>
2025-03-12 15:27:10 +0100

    Remove ui-vue-library

--- packages/ui-vue-library/src/index.ts
+++
@@ -1,9 +0,0 @@
-import './styles/style.css'
-export {DefaultTheme, PrimeVueConfig} from './styles/primevue'
-
-export {default as Footer} from './organisms/footer/Footer.vue'
-export {default as Navigation} from './organisms/navigation/Navigation.vue'
-
-export {default as ComposerPage} from './pages/composer/ComposerPage.vue'
-export {default as DashboardPage} from './pages/dashboard/DashboardPage.vue'
-export {default as HomePage} from './pages/home/HomePage.vue'
