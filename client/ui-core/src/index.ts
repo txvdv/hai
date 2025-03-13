a397e2529b578f4da044577e15ee702d72c0d56d
txvdv <txvdv@protonmail.com>
2025-03-13 13:31:15 +0100

    Add `getDocumentBrowserViewModel` method to `UiCore`
    Introduce a new method in `UiCore` to provide an instance of `DocumentBrowserViewModel`. This ensures that `UiCore` can now supply a pre-configured view model using the existing `DocumentService`.

--- client/ui-core/src/index.ts
+++ client/ui-core/src/index.ts
@@ -1,15 +1,24 @@
 import { Client } from './service/client.js';
 import { DocumentService } from './service/document-service.js';
+import { DocumentBrowserViewModel } from './document-browser/view-model.js';
 export * from './lib/ui-core.js';
 
 export class UiCore {
   private readonly client: Client;
+  private readonly documentService: DocumentService;
 
   constructor() {
     this.client = new Client();
+    this.documentService = new DocumentService(this.client);
   }
 
   getDocumentService() {
-    return new DocumentService(this.client);
+    return this.documentService;
+  }
+
+  getDocumentBrowserViewModel() {
+    return new DocumentBrowserViewModel({
+      documentService: this.documentService,
+    });
   }
 }
