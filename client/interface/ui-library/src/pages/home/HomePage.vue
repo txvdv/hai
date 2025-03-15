3a29dd6fce28446aa25f4f0efb6a2ba2974ec01f
txvdv <txvdv@protonmail.com>
2025-03-15 13:41:23 +0100

    Remove unused Navigation component from HomePage.vue
    The Navigation component and related navItems variable were removed as they are no longer used in the HomePage. This change simplifies the code and ensures only relevant components are included.

--- client/interface/ui-library/src/pages/home/HomePage.vue
+++ client/interface/ui-library/src/pages/home/HomePage.vue
@@ -1,27 +1,13 @@
 <script setup lang="ts">
-import Hero from "./Hero.vue";
-import Features from "./Features.vue";
-import Navigation from "../../organisms/navigation/Navigation.vue";
-
-const navItems = [{
-  text: 'HAI',
-  link: '/'
-}, {
-  text: 'Dashboard',
-  link: '/dashboard'
-}, {
-  text: 'Composer',
-  link: '/composer'
-}]
+import Hero from './Hero.vue';
+import Features from './Features.vue';
 </script>
 
 <template>
   <div>
-    <Navigation :items="navItems" />
     <Hero />
     <Features />
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
