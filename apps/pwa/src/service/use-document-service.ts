d135492b5bca44ffdc7400230011252c02e33b21
txvdv <txvdv@protonmail.com>
2025-03-15 19:58:21 +0100

    `Remove DocumentService and add helper for injection`
    The `DocumentService` class has been removed as it is no longer needed. A helper function, `useDocumentService`, was updated with documentation to clarify its purpose as a utility to retrieve the service via dependency injection.

--- apps/pwa/src/service/use-document-service.ts
+++ apps/pwa/src/service/use-document-service.ts
@@ -1,6 +1,10 @@
 import { inject } from 'vue';
 import { UiCore } from '@hai/ui-core';
 
+/**
+ * Not a stateful composable,
+ * but a helper to get the DocumentService from inject
+ */
 export function useDocumentService() {
   const core = inject('core') as UiCore;
   const documentService = core.getDocumentService();
